83dd61de4c6839451830d6f643aad81cda697311
Rahul Sonwanshi <rahulsonwanshi3@gmail.com>
2020-03-19 21:25:28 +0530

    added comments + refactoring

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { ReactiveFormsModule } from "@angular/forms";
-import * as Chart from 'chart.js';
 
 @Component({
   selector: 'home',
@@ -11,18 +9,10 @@ export class HomeComponent implements OnInit {
 
   ecoeff = 5;// coefficient of restitution
   
-  /*
-   * height1 = height0*e^2;
-   * s = ut + 1/2at^2;
-   * u = 0;
-   * t = (2s/g)^(1/2)
-   *
-   */
-  
   initialHeight = 100;
   timeaxis = [];
   heightaxis = [];
-  constructor() { }
+  bounces;
   public barChartOptions = {
     scaleShowVerticalLines: false,
     responsive: true
@@ -31,43 +21,59 @@ export class HomeComponent implements OnInit {
   public barChartType = 'line';
   public barChartLegend = true;
   public barChartData;
+
+   /*
+   * logic
+   * height1 is new height and height0 is initial height and e = ecoeff
+   * height1 = height0*e^2;
+   * s = ut + 1/2at^2;
+   * u = 0; since start at rest
+   * t = (2s/g)^(1/2) make time the subject
+   * 
+   */
+
+  constructor() { }
+  
   ngOnInit() {
     // console.log(this.ecoeff);
     this.newPlot();
   }
 
+  //plot the graph
   plotGraph() {
     while(Math.round((this.initialHeight + Number.EPSILON) * 100) / 100 != 0) {
       this.plotTime(this.initialHeight);
       this.plotHeight(this.initialHeight);
+      this.bounces++;
     }
   }
 
   plotTime(s) {
-    let time = Math.sqrt(2*s/10);
+    let time = Math.sqrt(2*s/10);  // using the above formula t = (2s/g)^(1/2)
     this.timeaxis.push(Math.round((time + Number.EPSILON) * 100) / 100);
   }
 
   plotHeight(h) {
-    let height = h*this.ecoeff/100*this.ecoeff/100;
+    let height = h*this.ecoeff/100*this.ecoeff/100; // using the above formula height1 = height0*e^2;
     this.heightaxis.push(height);
     this.initialHeight = height;
-    // console.log(Math.round((this.initialHeight + Number.EPSILON) * 100) / 100);
   }
 
+  // initialize paramters for graph plotting
   initGraph() {
     this.timeaxis = [];
     this.heightaxis = [];
     this.initialHeight = 100;
+    this.bounces = 0;
     this.timeaxis.push(0);
     this.barChartLabels = this.timeaxis;
     this.heightaxis.push(this.initialHeight);
-    // console.log(Math.round((this.initialHeight + Number.EPSILON) * 100) / 100);
     this.barChartData = [
       {data: this.heightaxis, label: 'Height Vs Time'},
     ];
   }
 
+  // start a new graph plot
   newPlot() {
     this.initGraph();
     this.plotGraph();
